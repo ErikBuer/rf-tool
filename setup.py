15919e9f4307022b01e4e2efed6f5151b90614b0
ErikBuer <buer94@gmail.com>
2019-11-30 20:08:02 +0100

    preparing for release 2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='rf-tool',
-      version='0.0.7',
+      version='0.0.8',
       description='RF electronics calculator',
       url='https://github.com/ErikBuer/rf-tool',
       author='Erik Buer',
