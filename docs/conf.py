fda4822242adf231cbd438ac5415ae81c650ab68
ErikBuer <buer94@gmail.com>
2020-08-03 07:19:13 +0200

    added master file specifier in conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -23,6 +23,8 @@ author = 'Erik Buer'
 
 
 # -- General configuration ---------------------------------------------------
+# Master document
+master_doc = 'index'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
